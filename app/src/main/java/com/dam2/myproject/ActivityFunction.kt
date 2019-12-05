460d52dd4cf30ed666aa077c674bda883df14485
aparcerozas <aparcerozas@danielcastelao.org>
2019-12-05 14:26:49 +0100

    Camara y Calculadora

--- app/src/main/java/com/dam2/myproject/ActivityFunction.kt
+++ app/src/main/java/com/dam2/myproject/ActivityFunction.kt
@@ -2,11 +2,40 @@ package com.dam2.myproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_function.*
 
 class ActivityFunction : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_function)
+
+        btnSuma.setOnClickListener{
+            val num1 = Integer.parseInt(txtNum1.text.toString())
+            val num2 = Integer.parseInt(txtNum2.text.toString())
+            val suma = num1 + num2
+            txtResult.setText(suma.toString())
+        }
+
+        btnResta.setOnClickListener{
+            val num1 = Integer.parseInt(txtNum1.text.toString())
+            val num2 = Integer.parseInt(txtNum2.text.toString())
+            val resta = num1 - num2
+            txtResult.setText(resta.toString())
+        }
+
+        btnProducto.setOnClickListener{
+            val num1 = Integer.parseInt(txtNum1.text.toString())
+            val num2 = Integer.parseInt(txtNum2.text.toString())
+            val producto = num1 * num2
+            txtResult.setText(producto.toString())
+        }
+
+        btnDivision.setOnClickListener{
+            val num1 = Integer.parseInt(txtNum1.text.toString())
+            val num2 = Integer.parseInt(txtNum2.text.toString())
+            val division = num1 / num2
+            txtResult.setText(division.toString())
+        }
     }
 }
--- app/src/main/java/com/dam2/myproject/MainActivity.kt
+++ app/src/main/java/com/dam2/myproject/MainActivity.kt
@@ -5,8 +5,23 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.browse
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.provider.MediaStore
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.jetbrains.anko.longToast
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 
-class MainActivity : AppCompatActivity() {
+
+
+val REQUEST_IMAGE_CAPTURE = 1
+
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,15 +33,39 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnMath.setOnClickListener{ view ->
-
+            val intent = Intent(this, ActivityFunction::class.java)
+            startActivity(intent)
         }
 
         btnCamera.setOnClickListener{ view ->
-
+            camera(view)
         }
     }
 
     private fun camera (y: View?) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.CAMERA)) {
+                longToast("Acceso a camara permitido")
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.CAMERA),0)
+            } else {
+                longToast("Permiso de camara rechazado")
+
+            }
+        } else {
+            longToast("Acceso a camara permitido")
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            val imageBitmap = data!!.extras!!.get("data") as Bitmap
+            imgCamera.setImageBitmap(imageBitmap)
+        }
 
     }
 }
